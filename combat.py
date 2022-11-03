8886d6a380f97c6f6dd1640f7a3158dd43d2a9d0
Shreyas Sridhar <shreyassridhar98@gmail.com>
2022-11-03 15:24:10 +0800

    Rewards Changed

--- combat.py
+++ combat.py
@@ -448,7 +448,7 @@ class Combat(gym.Env):
                             and opp_health[target_opp] > 0:
                         # Fire
                         opp_health[target_opp] -= 1
-                        rewards[agent_i] += 10
+                        rewards[agent_i] += 5
 
                         # Update agent cooling down
                         self._agent_cool[agent_i] = False
@@ -473,7 +473,7 @@ class Combat(gym.Env):
                             and agent_health[target_agent] > 0:
                         # Fire
                         agent_health[target_agent] -= 1
-                        rewards[target_agent] -= 1
+                        rewards[target_agent] -= 2
 
                         # Update opp cooling down
                         self._opp_cool[opp_i] = False
@@ -508,13 +508,13 @@ class Combat(gym.Env):
             #### Weidong's Edits v2 ####
             #### Reducing the rewards of individual agent by enemy team health AT END only
             for i in range(self.n_agents):
-                rewards[i] -= (0.1/self.n_agents)*sum([v for k, v in self.opp_health.items()])
+                rewards[i] -= sum([v for k, v in self.opp_health.items()])
                 #### Weidong's Edits v4 ####
                 #### Adding or Subtracting Reward if win
                 if (sum([v for k, v in self.opp_health.items()]) == 0):
-                    rewards[i] += (5.0/self.n_agents)
+                    rewards[i] += (50.0/self.n_agents)
                 if (sum([v for k, v in self.agent_health.items()]) == 0):
-                    rewards[i] -= (5.0/self.n_agents)
+                    rewards[i] -= (50.0/self.n_agents)
                 #$$$ End of Weidong's edits v4
             #$$$ End of Weidongs Edits v2
             self._agent_dones = [True for _ in range(self.n_agents)]
